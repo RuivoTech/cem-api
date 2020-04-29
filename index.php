e2ea92886a3c5ce310d5f2b0bcc722b6dab95385
Richieri Negri <ruivotech@hotmail.com>
2020-04-29 16:45:12 -0300

    Versionando Sistema Centro Evangélico de Maringá

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
+/*
+Versão: 1.2.0
 
+*/
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
